9d39d13db9907614de47b029ef68b7f3a957f547
Corey White <smortopahri@gmail.com>
2014-11-02 19:39:15 -0500

    Changed map service

--- app/js/map.js
+++ app/js/map.js
@@ -4,6 +4,7 @@
   	"esri/map", 
   	"esri/dijit/HomeButton",
   	"esri/dijit/BasemapGallery",
+  	"esri/dijit/Scalebar",
    	"esri/arcgis/utils",
    	"esri/layers/FeatureLayer", 
    	"esri/dijit/Legend",
@@ -14,22 +15,36 @@
     "dijit/TitlePane",
     "dijit/layout/AccordionContainer",
   	"dojo/domReady!"
-	], function(Map, HomeButton, BasemapGallery, arcgisUtils, FeatureLayer, Legend, arrayUtils, parser){
+	], function(Map, HomeButton, BasemapGallery, Scalebar, arcgisUtils, FeatureLayer, Legend, arrayUtils, parser){
 		 parser.parse();
+
+		 //variables
+		 var map,
+		 	home,
+		 	basemapGallery,
+		 	scalebar,
+		 	layerInfo,
+		 	legendDijit,
+		 	layers = [],
+	   		url,
+	   		temp;
+
+
+
 		//Set up init map view
-  		var map = new Map("map", {
+  	 	map = new Map("map", {
     		center: [-76.920236,  38.183921],
     		zoom: 13,
     		basemap: "national-geographic"
   		});
         //Add home button    
-  		var home = new HomeButton({
+  		home = new HomeButton({
     		map: map
   		}, "HomeButton");
   		home.startup();
 
   		//Adding basemap gallery
-	    var basemapGallery = new BasemapGallery({
+	    basemapGallery = new BasemapGallery({
 	      	showArcGISBasemaps: true,
 	        map: map
 	    }, "basemapGallery");
@@ -39,70 +54,42 @@
 	    	console.log("basemap gallery error:  ", msg);
 	    });
 
-	    var nonGameFish = new FeatureLayer("http://152.46.17.144/arcgis/rest/services/GEWA/gewa_sde/MapServer/0", {
-        	mode: FeatureLayer.MODE_ONDEMAND,
-        	outFields:["*"]
-      	});
-      	var gameFish = new FeatureLayer("http://152.46.17.144/arcgis/rest/services/GEWA/gewa_sde/MapServer/1", {
-        	mode: FeatureLayer.MODE_ONDEMAND,
-        	outFields:["*"]
-      	});
-      	var sampleLocations = new FeatureLayer("http://152.46.17.144/arcgis/rest/services/GEWA/gewa_sde/MapServer/2", {
-        	mode: FeatureLayer.MODE_ONDEMAND,
-        	outFields:["*"]
-      	});
-      	var fences = new FeatureLayer("http://152.46.17.144/arcgis/rest/services/GEWA/gewa_sde/MapServer/3", {
-        	mode: FeatureLayer.MODE_ONDEMAND,
-        	outFields:["*"]
-      	});
-      	var streams = new FeatureLayer("http://152.46.17.144/arcgis/rest/services/GEWA/gewa_sde/MapServer/4", {
-        	mode: FeatureLayer.MODE_ONDEMAND,
-        	outFields:["*"]
-      	});
-      	var boundary = new FeatureLayer("http://152.46.17.144/arcgis/rest/services/GEWA/gewa_sde/MapServer/5", {
-        	mode: FeatureLayer.MODE_ONDEMAND,
-        	outFields:["*"]
-      	});
-      	var building = new FeatureLayer("http://152.46.17.144/arcgis/rest/services/GEWA/gewa_sde/MapServer/6", {
-        	mode: FeatureLayer.MODE_ONDEMAND,
-        	outFields:["*"]
-      	});
-      	var roads = new FeatureLayer("http://152.46.17.144/arcgis/rest/services/GEWA/gewa_sde/MapServer/7", {
-        	mode: FeatureLayer.MODE_ONDEMAND,
-        	outFields:["*"]
-      	});
-      	var smallScaleFeatures = new FeatureLayer("http://152.46.17.144/arcgis/rest/services/GEWA/gewa_sde/MapServer/8", {
-        	mode: FeatureLayer.MODE_ONDEMAND,
-        	outFields:["*"]
-      	});
-      	var waterbodies = new FeatureLayer("http://152.46.17.144/arcgis/rest/services/GEWA/gewa_sde/MapServer/9", {
-        	mode: FeatureLayer.MODE_ONDEMAND,
-        	outFields:["*"]
-      	});
-      	var naturalVeg = new FeatureLayer("http://152.46.17.144/arcgis/rest/services/GEWA/gewa_sde/MapServer/10", {
-        	mode: FeatureLayer.MODE_ONDEMAND,
-        	outFields:["*"]
-      	});
-      	var culturalVeg = new FeatureLayer("http://152.46.17.144/arcgis/rest/services/GEWA/gewa_sde/MapServer/11", {
-        	mode: FeatureLayer.MODE_ONDEMAND,
-        	outFields:["*"]
-      	});
+	    //Scale bar
+	    scalebar = new Scalebar({
+          map: map,
+          scalebarUnit: "dual"
+        });
+
+	 
+
+	   	//Adds all layers to map and legend
+	    for (var i = 0; i < 12; i++ ){
+	   		url = "http://152.46.17.144/arcgis/rest/services/GEWA/gewa_sde/MapServer/" + i;
+	    	temp =  new FeatureLayer(url, {
+        		mode: FeatureLayer.MODE_ONDEMAND,
+        		outFields:["*"]
+      		});
+      		layers.push(temp);
+	    };
+	
 
      	//add the legend
      	map.on("layers-add-result", function (evt) {
-        	var layerInfo = arrayUtils.map(evt.layers, function (layer, index) {
+        	layerInfo = arrayUtils.map(evt.layers, function (layer, index) {
+        		console.log(layer.layer);
           		return {layer:layer.layer, title:layer.layer.name};
         	});
         	if (layerInfo.length > 0) {
-          		var legendDijit = new Legend({
+          		legendDijit = new Legend({
             		map: map,
-            		layerInfos: layerInfo
+            		layerInfos: layerInfo,
+            		respectCurrentMapScale: true
           	}, "legendDiv");
           	legendDijit.startup();
         	}
       	});
 
-      	map.addLayers([nonGameFish, gameFish, sampleLocations, fences, streams, boundary, building, roads, smallScaleFeatures, waterbodies, naturalVeg, culturalVeg]);
+      	map.addLayers(layers);
     
 
 
